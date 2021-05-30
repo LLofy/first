3f4edbd290dfecd843a48942753e22a04b7dc1f6
LLofy <45644153+LLofy@users.noreply.github.com>
2021-05-30 00:11:46 -0300

    Add files via upload

---
+++ subir/components/SmallCardDisplay.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h3 class="header">
+            {{cardsSection.title}}
+        </h3>
+        <div class="card-container">
+            <SmallCard
+                v-for="card in cardsSection.cards"
+                :key="card.id"
+                :card="card"
+            />
+        </div> 
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['cardsSection']
+    }
+</script>
+
+<style scoped>
+
+    .container {
+        position:relative;
+        top:240px;
+    }
+
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
